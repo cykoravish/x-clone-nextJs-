00781cfef577f655d3943f64ae2cbccc62bc9b3c
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-28 18:50:10 +0530

    add mini profile to sidebar if the user is authenticated

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -7,4 +7,15 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      console.log("ss: ", session);
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub; 
+      return session;
+    },
+  },
 };
