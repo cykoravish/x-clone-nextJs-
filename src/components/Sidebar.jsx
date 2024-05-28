00918a24ccee6be4a858297d25fca67436d92951
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-28 16:55:36 +0530

    next-auth with google added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoMdHome } from "react-icons/io";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col gap-4 p-3">
       <Link href="/">
@@ -16,9 +20,21 @@ export default function Sidebar() {
         <IoMdHome className="w-7 h-7" />
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold">
-        Sign In
-      </button>
+      {session ? (
+        <button
+          onClick={() => signOut()}
+          className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 }
