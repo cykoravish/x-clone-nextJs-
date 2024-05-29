b83046728815f07851188e1ad18abb95b6693f87
Ravish Bisht <ravishbisht85@gmail.com>
2024-05-29 11:43:58 +0530

    final commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,25 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./options";
+import NextAuth from 'next-auth';
 
-const handler = NextAuth(authOptions);
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(' ')
+        .join('')
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
--- src/components/SessionWrapper.jsx
+++ src/components/SessionWrapper.jsx
@@ -1,6 +1,14 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
+'use client';
 
-export default function SessionWrapper({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
+import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
+
+const SessionWrapper = ({ children }) => {
+  return (
+    <SessionProvider>
+      <RecoilRoot>{children}</RecoilRoot>
+    </SessionProvider>
+  );
+};
+
+export default SessionWrapper;
